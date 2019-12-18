c8fc6da98ce424fb27cad6727fe17d07f7e840cf
agalya17 <58978039+agalya17@users.noreply.github.com>
2019-12-18 18:22:38 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,64 @@
+#equal or not
+'''
+a=[10,20,30]
+b=[10,20,30]
+for i in range(len(a)):
+    if a[i]==b[i]:
+        print('yes')
+    else:
+        print('not')
+'''
+#factorial(5*4*3*2*1)
+'''
+a=int(input('enter the value'))
+fact=1
+for i in range(1,a+1,):
+        fact=fact*i
+print(fact)
+'''
+#factorial(reverse1*2*3*4*5))
+'''
+a=int(input('enter the value'))
+fact=1
+for i in range(a,0,-1):
+        fact=fact*i
+print(fact)
+'''
+#fibanacci
+'''
+a=int(input('enter the a value'))
+b=int(input('enter the b value'))
+for i in range(0,7):
+    c=a+b
+    a=b
+    b=c
+    print(c)
+'''
+#prime number
+'''a=int(input('enter the a value'))
+for i in range(2,a):
+    if a%i==0:
+      print(' not prime')
+    else:
+        print('prime')
+        break
+'''
+a='abc$123$#92fg'
+for i in a:
+    if i.isdigit():
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
