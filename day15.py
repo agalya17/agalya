9b75a8da88ee8bd05ce7a5539e6eebb8a0fcb0cc
agalya17 <58978039+agalya17@users.noreply.github.com>
2020-01-06 19:13:47 +0530

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,38 @@
+#matrix
+'''
+x=int(input('enter the number:'))
+a=[[0 for i in range(x)]for j in range(x)]
+for i in range(3):
+    for j in range(3):
+        print(a[i][j],end=' ')
+    print()
+'''
+#pattern program
+'''
+num=5
+i=1
+while i<=5:
+    print('*' * i)
+    i+=1
+'''
+'''
+num=5
+for i in range(num):
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+'''
+'''
+num=5
+for i in range(num):
+    for j in range(i+1):
+        print(i+1,end=' ')
+    print()
+'''
+'''
+num=5
+for i in range(num):
+    for j in range(i+1):
+        print(j+1,end=' ')
+    print()
+'''
