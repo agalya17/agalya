0dcbeef0fae2474771391d0ca05bc33e57b99c55
agalya17 <58978039+agalya17@users.noreply.github.com>
2020-01-07 19:18:03 +0530

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,67 @@
+'''
+num=5
+n=1
+for i in range(num):
+    for j in range(i+1):
+        print(n,end=' ')
+        n=n+1
+    print()
+'''
+'''
+num=5
+for i in range(num,0,-1):
+    for j in range(i):
+        print(j+1,end=' ')
+    print()
+'''
+'''
+num=5
+k=1
+for i in range(num):
+    for j in range(k):
+        print(j+1,end=' ')
+    k=k+2
+    print()
+'''
+'''
+num=5
+for i in range(1,num+1):
+    i=i*2-1
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+'''
+'''
+num=5
+for i in range(1,num+1):
+    k=64
+    for j in range(i):
+        print(chr(k+1),end=' ')
+        k=k+1
+    print()
+'''
+'''
+num=5
+for i in range(1,num+1):
+    for j in range(i):
+        print(chr(65+j),end=' ')
+    print()
+'''     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
